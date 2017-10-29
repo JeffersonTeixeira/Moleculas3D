b02f2b5e8a12899a22e29f1d5bd345048a6abb0a
jefferson <jeffersonteixeira@outlook.com>
2017-10-29 13:27:47 -0200

    fileExplorer

--- android/src/br/com/syssolutions/moleculas3d/AndroidLauncher.java
+++ android/src/br/com/syssolutions/moleculas3d/AndroidLauncher.java
@@ -1,6 +1,9 @@
 package br.com.syssolutions.moleculas3d;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -8,17 +11,29 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 
 public class AndroidLauncher extends AndroidApplication implements br.com.syssolutions.moleculas3d.control.Moleculas3DCallBack {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new br.com.syssolutions.moleculas3d.model.Moleculas3D(this), config);
-	}
 
-	@Override
-	public void startActivity() {
-		Intent intent = new Intent(this, Biblioteca.class);
-		startActivity(intent);
 
-	}
+    public int RESULTPERMISSION;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, RESULTPERMISSION);
+            }
+        }
+
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new br.com.syssolutions.moleculas3d.model.Moleculas3D(this), config);
+
+    }
+
+    @Override
+    public void startActivity() {
+        Intent intent = new Intent(this, Biblioteca.class);
+        startActivity(intent);
+
+    }
 }
---
+++ core/src/br/com/syssolutions/moleculas3d/view/FileExplorerState.java
@@ -0,0 +1,224 @@
+package br.com.syssolutions.moleculas3d.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+
+import br.com.syssolutions.moleculas3d.Utils.FontGenerator;
+import br.com.syssolutions.moleculas3d.control.states.GameStateManager;
+import br.com.syssolutions.moleculas3d.control.states.State;
+import br.com.syssolutions.moleculas3d.model.Moleculas3D;
+
+/**
+ * Created by jefferson on 24/10/17.
+ */
+
+public class FileExplorerState extends State {
+
+    private OrthographicCamera camOrtho;
+
+
+    private String extRoot, locRoot;
+    private FileHandle dirHandle;
+
+    private FileHandle[] listFiles;
+
+
+    private Texture background;
+    private Skin skin;
+    private TextureAtlas textureAtlas;
+
+    private Texture folderT, fileT, molT;
+
+    private Table container;
+
+    private Stage stage;
+    private ScrollPane scrollpane;
+    private LabelStyle labelStyle;
+
+
+    public FileExplorerState(GameStateManager gsm) {
+        super(gsm);
+
+        camOrtho = new OrthographicCamera();
+        camOrtho.setToOrtho(false, Moleculas3D.WIDTH, Moleculas3D.HEIGHT);
+
+
+        //Initial Patch:
+        if (Gdx.files.isExternalStorageAvailable()) {
+            extRoot = Gdx.files.getExternalStoragePath();
+            System.out.println("external Exists: " + extRoot);
+        }
+        if (Gdx.files.isLocalStorageAvailable()) {
+            locRoot = Gdx.files.getLocalStoragePath();
+
+//            FileHandle file = Gdx.files.local("myfile.txt");
+//            file.writeString("My god, it's full of stars", false);
+
+            System.out.println("local Exists: " + locRoot);
+        }
+
+        skin= new Skin();
+
+
+
+        folderT = new Texture(Gdx.files.internal("ui-imagens/folder.png"));
+        fileT = new Texture(Gdx.files.internal("ui-imagens/file.png"));
+        molT = new Texture(Gdx.files.internal("ui-imagens/mol.png"));
+
+
+
+//Internal é do APP
+
+        // dirHandle = Gdx.files.external(extRoot);
+        dirHandle = Gdx.files.external("");
+
+
+//        if (dirHandle.isDirectory()) {
+//            for (FileHandle file : dirHandle.list()) {
+//                System.out.println(file);
+//            }
+//        } else {
+//            System.out.println("Não é diretório");
+//        }
+
+
+
+        background = new Texture("ui-imagens/background.jpg");
+
+        labelStyle = new Label.LabelStyle();
+
+        FontGenerator fontTitulo = new FontGenerator(40, "VeraBd.ttf",null);
+
+        labelStyle.font = fontTitulo.getFont();
+
+
+
+       showFiles(dirHandle);
+
+    }
+
+
+    private void carregaPath(FileHandle fileHandle) {
+
+
+    }
+
+
+    public void showFiles(FileHandle fileHandle) {
+
+        container = new Table();
+        container.setWidth(Gdx.graphics.getWidth());
+        container.setHeight(Gdx.graphics.getHeight());
+
+
+        Table innerContainer = new Table();
+        //innerContainer.setWidth(container.getWidth() * (0.85f));
+        innerContainer.setWidth(Gdx.graphics.getWidth());
+        innerContainer.setHeight(Gdx.graphics.getHeight());
+
+
+        if (fileHandle.isDirectory()) {
+            for (FileHandle file : fileHandle.list()) {
+
+                Table table = new Table(skin);
+                table.setWidth(Gdx.graphics.getWidth());
+                table.setHeight(Gdx.graphics.getHeight());
+
+
+                if (file.isDirectory()) {
+
+                    table.add(new Image(folderT)).expandY().fillY();
+                    table.add(new Label("", labelStyle)).width(10f).expandY().fillY();// a spacer
+                    table.add(new Label(file.name(), labelStyle)).expandY().fillY();
+
+                    innerContainer.add(table).expand().fill();
+                    innerContainer.row();
+
+                } else {
+                    if (file.extension() != "mol") {
+                        table.add(new Image(fileT)).expandY().fillY();
+
+                    } else {
+                        table.add(new Image(molT)).expandY().fillY();
+                    }
+
+                    table.add(new Label("", labelStyle)).width(10f).expandY().fillY();// a spacer
+                    table.add(new Label(file.name(), labelStyle)).expandY().fillY();
+
+
+                    innerContainer.add(table).expand().fill().align(8);
+                    innerContainer.row();
+
+                }
+
+
+                System.out.println(file);
+            }
+        } else {
+            System.out.println("Não é diretório");
+        }
+
+
+
+        scrollpane = new ScrollPane(innerContainer);
+        scrollpane.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        scrollpane.setScrollingDisabled(true, false);
+
+        container.add(scrollpane).fill().expand();
+
+
+
+        stage = new Stage();
+
+
+        stage.addActor(container);
+
+        Gdx.input.setInputProcessor(stage);
+
+
+    }
+
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+        sb.setProjectionMatrix(camOrtho.combined);
+        sb.begin();
+        //sb.draw(background, 0, 0, camOrtho.viewportWidth, camOrtho.viewportHeight);
+        sb.end();
+
+
+        stage.act();
+        stage.draw();
+
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
